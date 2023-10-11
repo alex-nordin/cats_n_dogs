11b526f2000b1385ff8ef095337443e94f437949
Alex Nordin <alex_mc_nordin@proton.me>
2023-10-11 20:18:01 +0200

    Add first attempt at dataset import

--- src/cats_n_dogs.jl
+++ src/cats_n_dogs.jl
@@ -1,5 +1,28 @@
 module cats_n_dogs
 
-greet() = print("Hello World!")
+using Flux
+using Flux:update!
+using DataAugmentation
+using Metalhead
+using Images
+
+function createDataset(path)
+    X = []
+    y = []
+    for label in readdir(path)
+        for file in readdir("$path/$label")
+            img = load("$path/$label/$file")
+            im_size = (224, 224)
+            transform = DataAugmentation.compose(ScaleKeepAspect(im_size), CenterCrop(im_size))
+            #data = reshape(Float32.(channelview(img)),28,28,1)
+            _data = itemdata(apply(transform, Image(img)))
+            _data = collect(channelview(float32.(RGB.(_data))))
+            data = permutedims(_data, (3, 2, 1))
+            push!(X, data)
+            push!(y, label)
+        end
+    end
+    return X,Flux.onehotbatch(y, ["cats", "dogs"])
+end
 
 end # module cats_n_dogs
