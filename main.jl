990e8683770f17186a204112345834600ed16c90
Alex Nordin <alex_mc_nordin@proton.me>
2023-11-28 22:37:25 +0100

    Add comments

--- main.jl
+++ main.jl
@@ -6,8 +6,10 @@ using CUDA
 using cuDNN
 using ArgParse
 
+#set device variable as either gpu ot cpu depending on if a working cuda installation is present
 device = CUDA.functional() ? gpu : cpu
 
+#set an optional flag for number of epochs to train model, and parse what has been passed
 function parse_cli(ARGS)
 	settings = ArgParseSettings()
     @add_arg_table settings begin
@@ -21,13 +23,17 @@ end
 
 args = parse_cli(ARGS)
 
+#grab epochs variable from args
 const EPOCHS = args["epochs"]
 
+#create a dataset and load it into memory, then split it into training and validation sets at a 07/03 split
 const dataset_x, dataset_y = createDataset("./src/data/train")
 const training_set, validation_set = splitobs((dataset_x, dataset_y), at=0.7, shuffle=true)
 
+#set batchzise to something fairly small as I'm running an older gpu locally
 const BATCHSIZE = 16
 
+#define variables for our two dataloaders. paralell makes it concurrent, collate puts all arrays in a batch into one array
 training_iterator = Flux.DataLoader(
     training_set,
     batchsize = BATCHSIZE,
@@ -42,14 +48,19 @@ validation_iterator = Flux.DataLoader(
     parallel = true,
 )
 
+#if cuda is available, make dataloaders CuIterators for increased efficiency
 device == gpu ? val_iter = CUDA.CuIterator(validation_iterator) : nothing
 device == gpu ? train_iter = CUDA.CuIterator(training_iterator) : nothing
 
+# the base of our model is a pre-trained resnet model. omit the output layer and send to gpu or cpu
 model_base = Metalhead.ResNet(18, pretrain = true).layers[1:end-1] |> device
+#very simple model to fine-tune our results for current use case.
 model_tune = Chain(AdaptiveMeanPool((1,1)), Flux.flatten, Dense(512 => 3)) |> device
 
+#setup Adam optimizer
 opt = Flux.setup(Flux.Optimisers.Adam(1e-3), model_tune)
 
+#training loop
 for iter = 1:EPOCHS
     @time train_epoch!(model_base, model_tune; opt, train_iter)
     metric_train = evaluate(model_base, model_tune, train_iter)
